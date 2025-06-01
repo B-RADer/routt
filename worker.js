3f5ab3bd5d7765932b7e53af12e6e8adc370bad3
B-RADer <119801162+B-RADer@users.noreply.github.com>
2025-05-31 18:27:17 -0600

    fixed naming

--- worker.js
+++ worker.js
@@ -5,7 +5,7 @@ export default {
     if (path === '/' || path === '') path = '/index.html';
     if (!path.includes('.')) path += '.html';
     try {
-      const asset = await env.ASSETS.fetch(new Request(`https://routt-distillery.bradleychr.workers.dev${path}`));
+      const asset = await env.ASSETS.fetch(new Request(`https://routt.bradleychr.workers.dev${path}`));
       return asset;
     } catch (e) {
       return new Response('Not Found', { status: 404 });

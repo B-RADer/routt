303b4cbff4490f0446064480d0d4fc66efa56a1c
B-RADer <119801162+B-RADer@users.noreply.github.com>
2025-06-11 15:08:57 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,14 +1,5 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-    let path = url.pathname;
-    if (path === '/' || path === '') path = '/index.html';
-    if (!path.includes('.')) path += '.html';
-    try {
-      const asset = await env.ASSETS.fetch(new Request(`https://routt.bradleychr.workers.dev${path}`));
-      return asset;
-    } catch (e) {
-      return new Response('Not Found', { status: 404 });
-    }
+    return env.ASSETS.fetch(request);
   }
 };
